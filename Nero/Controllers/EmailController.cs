d09cd4e1045e1d2192b38eee0728954768538eb7
asven <asvennes@gmail.com>
2015-03-01 15:31:20 -0600

    actually changing language finally

---
+++ Nero/Controllers/EmailController.cs
@@ -0,0 +1,67 @@
+﻿using Nero.Model;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nero.Controllers
+{
+    public class EmailController : Controller
+    {
+        //
+        // GET: /Email/
+
+        [HttpPost]
+        public ActionResult Index(EmailUser emailUser)
+        {
+            SmtpClient client = new SmtpClient("s18227524.onlinehome-server.com");
+            MailMessage message = new MailMessage();
+
+
+            message.To.Add(ConfigurationManager.AppSettings["contactEmail"].ToString());
+            message.From = new MailAddress("info@neroeng.com");
+            message.Subject = "Contact form completed on neroeng.com";
+            message.IsBodyHtml = true;
+
+            StringBuilder html = new StringBuilder();
+            html.Append("<table>");
+            html.Append("<tr>");
+            html.Append("<td>");
+            html.Append("Name");
+            html.Append("</td>");
+            html.Append("<td>");
+            html.Append(emailUser.Name);
+            html.Append("</td>");
+            html.Append("</tr>");
+            html.Append("<tr>");
+            html.Append("<td>");
+            html.Append("Email Address");
+            html.Append("</td>");
+            html.Append("<td>");
+            html.Append(emailUser.EmailAddress);
+            html.Append("</td>");
+            html.Append("</tr>");
+            html.Append("<tr>");
+            html.Append("<td>");
+            html.Append("Message");
+            html.Append("</td>");
+            html.Append("<td>");
+            html.Append(emailUser.Message);
+            html.Append("</td>");
+            html.Append("</tr>");
+            html.Append("</table>");
+
+            message.Body = html.ToString();
+
+            client.Send(message);
+
+
+            return View();
+        }
+
+    }
+}
--- Nero/Global.asax.cs
+++ Nero/Global.asax.cs
@@ -21,7 +21,6 @@ namespace Nero
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            AuthConfig.RegisterAuth();
         }
     }
 }

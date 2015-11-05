65823aec7461df562c661d0738cd9792fcd46871
asven <asvennes@gmail.com>
2015-11-05 15:25:35 -0600

    ading

---
+++ Nero.Models/EmailUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Nero.Models
+{
+    public class EmailUser
+    {
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [DisplayName("Email Address")]
+        public string EmailAddress { get; set; }
+
+        [DisplayName("Message")]
+        public string Message { get; set; }
+    }
+}
---
+++ Nero/Controllers/AboutController.cs
@@ -0,0 +1,37 @@
+﻿using Nero.DataAccess;
+using Nero.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nero.Controllers
+{
+    public class AboutController : Controller
+    {
+        //
+        // GET: /About/
+
+
+        public ActionResult Index(string name)
+        {
+            if(string.IsNullOrEmpty(name))
+            {
+                return View(EmployeeManager.GetAllEmployees());
+            }
+            else
+            {
+                Employee employee = EmployeeManager.GetEmployeeByFirstName(name);
+                if(employee == null)
+                {
+                    return View(EmployeeManager.GetAllEmployees());
+                }
+                List<Employee> employeeForView = new List<Employee>();
+                employeeForView.Add(employee);
+                return View(employeeForView);
+            }
+            
+        }
+    }
+}
